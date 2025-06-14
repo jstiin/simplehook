9589f3f31e1b7ce7d371df25f26388f6bad39190
Justin <186339966+jstiin@users.noreply.github.com>
2025-06-14 02:54:50 +0200

    Refactor code structure for improved readability and maintainability

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open(os.path.join(this_directory, "README.md"), encoding="utf-8") as f:
 
 setup(
     name="simplehook",
-    version="1.4.2",
+    version="1.4.3",
     description="Simple Discord webhook wrapper",
     long_description=long_description,  
     long_description_content_type="text/markdown", 
@@ -15,10 +15,14 @@ setup(
     url="https://github.com/jstiin/simplehook",
     packages=find_packages(),
     install_requires=["aiofiles", "httpx"],
+    extra_requres={
+        "dev": ["pytest", "pytest-cov", "pytest-async"]
+    },
     python_requires='>=3.7',
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
+        "License :: OSI Approved :: GNU Affero General Public License v3",
     ],
     keywords="discord webhook messaging",
+    license="AGPL-3.0-only"
 )
